a1da4a4d2588db05068aa56b44df52d8f9758a83
Subramanian <gokusubr@amazon.com>
2018-09-07 17:28:50 +0100

    Initial commit

---
+++ common.sh
@@ -0,0 +1,40 @@
+# Setting up standard path for configuration.
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# Managing Linux packages in the absence of brew or another
+# package manager.
+if [[ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
+  source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+fi
+
+# Setting up editor.
+export EDITOR=`which nvim`
+alias vim="$EDITOR"
+
+# Fuzzy searching with FZF.
+if [[ -f "$HOME/.fzf.zsh" ]]; then
+  source "$HOME/.fzf.zsh"
+fi
+
+# Forcing SSH protocol version 2 which is safer.
+ssh() {
+    /usr/bin/ssh -2 $*;
+}
+
+# Going up the directory tree.
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+alias ..5="cd ../../../../.."
+alias ..6="cd ../../../../../.."
+alias ..7="cd ../../../../../../.."
+
+# Creating a directory and cd'ing into it.
+function mkcd() {
+    mkdir -p "$1" && cd "$1";
+}
+
+# Groovy.
+export GROOVY_HOME="/usr/local/opt/groovy/libexec"
+
