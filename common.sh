4906ef251f726004fea1362f539d32fc8666c451
Gokul Ramanan Subramanian <gokusubr@amazon.com>
2020-06-28 15:33:57 +0100

    Setup pyenv

--- common.sh
+++ common.sh
@@ -51,3 +51,9 @@ function mkcd() {
     mkdir -p "$1" && cd "$1";
 }
 
+# Setting up pyenv.
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
